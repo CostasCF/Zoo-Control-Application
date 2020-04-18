7dd30662ae3699cb8232340d678bcb3ee30c0345
Konstantinos Kalogeropoulos <kkalo878@gmail.com>
2020-04-18 04:44:34 +0300

    Update showAnimals.java

--- 2ndSemester_Assignment/src/showAnimals.java
+++ 2ndSemester_Assignment/src/showAnimals.java
@@ -13,13 +13,13 @@ public class showAnimals extends addAnimal {
 	}
 	
 	public void PrintA() {
-		System.out.println("|-----------------------------------------|");
-		System.out.println("| ID of the animal:" + id);               
-		System.out.println("| Name:" + name);
-		System.out.println("| Biology Class:" + BelongClass );
-		System.out.println("| Avg weight:" + weight );
-		System.out.println("| Max life span:" + age);
-		System.out.println("|-----------------------------------------|");
+		System.out.println("-----------------------------------------");
+		System.out.println(" ID of the animal:" + id);               
+		System.out.println(" Name:" + name);
+		System.out.println(" Biology Class:" + BelongClass );
+		System.out.println(" Avg weight:"+ weight );
+		System.out.println(" Max life span:" + age);
+		System.out.println("-----------------------------------------");
 	}
 
 }
