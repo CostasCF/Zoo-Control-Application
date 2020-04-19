ceb08b0b7f4687e03fd97d28f63f75093c182221
Konstantinos Kalogeropoulos <kkalo878@gmail.com>
2020-04-19 20:58:15 +0300

    Added serialization!

--- 2ndSemester_Assignment/src/addAnimal.java
+++ 2ndSemester_Assignment/src/addAnimal.java
@@ -1,23 +1,26 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.*; 
 
-public class addAnimal extends Menu{ //inherits from menu class the major creation of the ArrayList
+public class addAnimal extends Menu implements Serializable{ //inherits from menu class the major creation of the ArrayList
+
 	int id;
 	String name;
 	String BelongClass;
 	double weight;
 	double age;
 	
-
+	String filename = "file.ser"; 
 	static Scanner scan = new Scanner(System.in);
 	static AnimalValidationandInput av = new AnimalValidationandInput();
 	
 	//this method enters the user's animal data in a new intetity inside the ArrayList
 	public void newAnimal() {
 		System.out.println("You chose the 2nd option, please enter a new animal by adding its details one by one!");
+		//---- Id of the animal's input-------------------------------------------------------
 		System.out.println("Enter the id of the animal");
 		int id = av.inputValidInt();
-		
+		//-----Name of the Animal input----------------------------------------------------------------
 		System.out.println("Enter the name of the animal.");
 		boolean flag = false;
 		String name = "" ; // handling name input
@@ -28,23 +31,46 @@ public class addAnimal extends Menu{ //inherits from menu class the major creati
 				System.out.println("Please enter valid characters");
 			}
 		}
-		
+		//----------Biology class input----------------------------------------------------------------
 		System.out.println("Enter the biology class of the animal.");
 		flag = false;
 		String BelongClass = ""; //handling biology class input
-		while (!flag) {
+		while (!flag) { 
 			BelongClass = scan.nextLine();
 			flag = av.StringExceptionHandling(BelongClass);
 			if (flag == false) {
 				System.out.println("Please enter valid characters");
 			}
 		}
+		//--------weight and age input--------------------------------------------------------
 		System.out.println("Enter the weight of the animal.");
 		double weight = av.inputValidDouble();
 		System.out.println("Enter the age of the animal.");
 		double age = av.inputValidDouble();
 		Animal animal = new Animal(id, name, BelongClass, weight, age);
 		Catalog.add(animal);
+		        // Serialization  
+				try
+				{    
+					//Saving of object in a file 
+					FileOutputStream file = new FileOutputStream(filename); 
+					ObjectOutputStream out = new ObjectOutputStream(file); 
+					  
+					// Method for serialization of object 
+					out.writeObject(Catalog); 
+					  
+					out.close(); 
+					file.close(); 
+					  
+					System.out.println("Object has been serialized"); 
+		  
+				} 
+				  
+				catch(IOException ex) 
+				{ 
+					System.out.println("IOException is caught"); 
+				} 
+		  
 	}
 	
 
--- 2ndSemester_Assignment/src/showAnimals.java
+++ 2ndSemester_Assignment/src/showAnimals.java
@@ -1,15 +1,44 @@
 import java.util.ArrayList;
+import java.io.*; 
 
-public class showAnimals extends addAnimal {
+
+
+public class showAnimals extends addAnimal  implements Serializable {
 
 	
+	@SuppressWarnings("unchecked")
 	public void Show() {
-		int Arraysize = Catalog.size();
-		int i = 0;
-		while (i < Arraysize) {
-			Catalog.get(i).PrintA();
-			i++;
-		}
+		addAnimal animal = null; 
+		 // Deserialization 
+		 try
+		 {    
+			 // Reading the object from a file 
+			 FileInputStream file = new FileInputStream(filename); 
+			 ObjectInputStream in = new ObjectInputStream(file); 
+			   
+			 // Method for deserialization of object 
+			 Catalog = (ArrayList)in.readObject(); 
+
+			   
+			 System.out.println("Object has been deserialized ");
+			 int Arraysize = Catalog.size();
+				int i = 0;
+				while (i < Arraysize) {
+					Catalog.get(i).PrintA();
+					i++; }
+			 in.close(); 
+			 file.close(); 
+
+			 }
+		  catch(IOException ex) 
+			 { 
+				 System.out.println("IOException is caught"); 
+			 } 
+	        catch(ClassNotFoundException ex) 
+	        { 
+	            System.out.println("ClassNotFoundException is caught"); 
+	        } 
+	  
 	}
 	
 	public void PrintA() {
